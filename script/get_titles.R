c110b2933cb628eaddcfcb1ed60a9a21f8e5f89a
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-26 00:00:14 +0200

    Get country where the journal is published

--- script/get_titles.R
+++ script/get_titles.R
@@ -1,8 +1,7 @@
-#Get title
+#Get an article title
 article_title <- xml_find_all(summary, "//ArticleTitle") %>% 
   xml_text(trim = TRUE)
 
-
-#Get journal title 
+#Get journal title in which an article was published
 journal_title <- xml_find_first(summary, ".//Journal/Title") %>% 
   xml_text()

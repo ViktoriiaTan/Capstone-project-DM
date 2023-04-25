0c3db8e6fcf004bc174074384b81488d2641609b
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-25 23:58:59 +0200

    Retrieving doi

--- script/pubmed_funcs.R
+++ script/pubmed_funcs.R
@@ -16,7 +16,6 @@ query <- gsub(" ", "+", query)
 #Search for articles ids
 pubmed_ids <- function(db = "pubmed", query, retmax= 20){
   url <- glue("https://www.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db={db}&term={query}&usehistory=y&retmax={retmax}&api_key={api_key}")
-  print(url)
   response <- httr::GET(url)
   doc <- read_xml(response)
   ids <- xml_find_all(doc, ".//Id") %>% 
@@ -24,22 +23,23 @@ pubmed_ids <- function(db = "pubmed", query, retmax= 20){
   
   return(ids)
 }
-art_ids <- ncbi_ids(db, "sepsis", 4)
+art_ids <- pubmed_ids(db, "endometriosis", 4)
 
 #
 pubmed_abstract <- function(db , ids = NULL, 
-                         retmode = "abstract", rettype = "text") {
+                         retmode = "abstract", rettype = "xml") {
   ids_str <- paste(ids, collapse = ",")
   url <- glue("https://www.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db={db}&id={ids_str}&retmode={retmode}&rettype={rettype}&api_key={api_key}")
-  print(url)
+
   response <- httr::GET(url)
   doc <- read_xml(response)
+  
   abstracts <- xml_find_all(doc, ".//AbstractText") %>% 
     xml_text()
   
   return(abstracts)
 }
-abstracts <- ncbi_abstract(db, art_ids)
+abstracts <- pubmed_abstract(db, art_ids)
 
 pubmed_linkout <- function(db, ids) {
   url <- glue("https://www.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom={db}&id={ids}&cmd=lcheck&api_key={api_key}")
@@ -64,4 +64,6 @@ pubmed_linkout <- function(db, ids) {
 
 pubmed_linkout(db, 2011359)
 
-
+#Retrieving doi
+doi <- xml_find_all(summary, ".//ArticleId[@IdType='doi']") %>%
+  xml_text()

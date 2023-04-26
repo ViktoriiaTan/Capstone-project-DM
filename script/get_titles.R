11aec96ba8d7e11953458327fd172d9667a3c7aa
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-26 23:43:54 +0200

    abstract code fixes & improvements

--- script/get_titles.R
+++ script/get_titles.R
@@ -11,7 +11,29 @@ journal_title <- xml_find_all(summary, ".//Journal/Title") %>%
 abstracts <- xml_find_all(summary, "//Article/Abstract") %>%
   xml_text()
 
+abstract_function <- function(summary){
+  
+  values <- c()
+  
+  articles <- xml_find_all(summary, "//Article")
+  
+  for (i in 1:length(articles)) {
+    article <- articles[[i]]
+    abstract_element <- xml_find_first(article, "Abstract")
+    
+    if (is.null(abstract_element)){
+      values <- c(values, "No abstract")
+      next
+    }
+    
+    values <- c(values, xml_text(abstract_element))
+  }
+  return(values)
+}
+
+abstract <- abstract_function(summary)
+
 #Get country where the journal is published
-country <- xml_find_all(summary, ".//Country") %>% 
+country <- xml_find_all(summary, ".//MedlineJournalInfo/Country") %>% 
   xml_text()
 

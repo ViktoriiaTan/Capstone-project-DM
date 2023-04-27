0654cce25851bcf2b71e20f36ef06f4df092c5c6
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-27 17:11:49 +0200

    rename file and move some func-s

---
+++ script/items.R
@@ -0,0 +1,45 @@
+#Get authors
+authors <- xml_find_all(articles, "//Author") %>%
+  lapply(function(x) {
+    last_name <- xml_find_all(x, ".//LastName") %>% xml_text(trim = TRUE)
+    initials <- xml_find_all(x, ".//Initials") %>% xml_text(trim = TRUE)
+    paste(last_name, initials, sep = " ")
+  }) %>%
+  paste(collapse = ", ")
+
+# Function to get authors from xml
+get_authors <- function(articles){
+  authors <- xml_find_all(articles, "//Author") %>%
+    lapply(function(x) {
+      last_name <- xml_find_all(x, ".//LastName") %>% xml_text(trim = TRUE)
+      initials <- xml_find_all(x, ".//Initials") %>% xml_text(trim = TRUE)
+      paste(last_name, initials, sep = " ")
+    }) %>%
+    paste(collapse = ", ")
+  return(authors)
+}
+authors <- get_authors(articles)
+
+#Get country where the journal is published
+get_country <- function(articles){
+  country <- xml_find_all(articles, ".//MedlineJournalInfo/Country") %>% 
+    xml_text()
+  return(country)
+}
+country <- get_country(articles)
+country <- xml_find_all(articles, ".//MedlineJournalInfo/Country") %>% 
+  xml_text()
+
+# Retrieve the DOI for a given PubMed article
+doi_results <- function(art){
+  pubmed_articles <- xml_find_all(art, ".//PubmedArticle")
+  lapply(pubmed_articles, function(article) {
+    doi <- xml_find_first(article, ".//PubmedData/ArticleIdList/ArticleId[@IdType='doi']")
+    if (is.null(doi)) {
+      return()
+    } else {
+      return(xml_text(doi))
+    }
+  })
+}
+doi <- doi_results(articles)

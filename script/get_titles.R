5d115dca547911ac4ee7cf00671f5189c8310918
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-27 15:15:14 +0200

    fixxing errors in titles

--- script/get_titles.R
+++ script/get_titles.R
@@ -1,21 +1,66 @@
 #Get an article title
-article_title <- xml_find_all(summary, "//ArticleTitle") %>% 
-  xml_text(trim = TRUE)
+
+title <- function(art){
+  all_art <- xml_find_all(art, "//PubmedArticle")
+  num_docs <- length(all_art)
+  
+  titles <- lapply(all_art, function(pubmed_article){
+    article_node <- xml_find_first(pubmed_article, ".//Article")
+    
+    if (is.null(article_node)) {
+      return("NA")
+    } else {
+      title <- xml_find_first(article_node, ".//ArticleTitle")
+      
+      if (is.null(title)) {
+        return("NA")
+      } else {
+        return(xml_text(title, trim = TRUE))
+      }
+    }
+  })
+  
+  return(titles)
+}
+article_title <- title(articles)
+
 
 #Get journal title in which an article was published
-journal_title <- xml_find_all(summary, ".//Journal/Title") %>% 
-  xml_text()
+
+journal_t <- function(art){
+  all_art <- xml_find_all(art, "//PubmedArticle")
+  num_docs <- length(all_art)
+  
+  j_titles <- lapply(all_art, function(pubmed_article){
+    journal_node <- xml_find_first(pubmed_article, ".//Journal")
+    
+    if (is.null(journal_node)) {
+      return("NA")
+    } else {
+      title <- xml_find_first(journal_node, ".//Title")
+      
+      if (is.null(title)) {
+        return("NA")
+      } else {
+        return(xml_text(title, trim = TRUE))
+      }
+    }
+  })
+  
+  return(j_titles)
+}
+journal_title <- journal_t(articles)
 
 #Retrieve abstracts
 
-abstracts <- xml_find_all(summary, "//Article/Abstract") %>%
+abstracts <- xml_find_all(articles, "//Article/Abstract") %>%
   xml_text()
 
-abstract_function <- function(summary){
+abstract_function <- function(art){
   
   values <- c()
   
-  articles <- xml_find_all(summary, "//Article")
+  articles <- xml_find_all(art, "//Article")
   
   for (i in 1:length(articles)) {
     article <- articles[[i]]
@@ -31,9 +76,9 @@ abstract_function <- function(summary){
   return(values)
 }
 
-abstract <- abstract_function(summary)
+abstract <- abstract_function(articles)
 
 #Get country where the journal is published
-country <- xml_find_all(summary, ".//MedlineJournalInfo/Country") %>% 
+country <- xml_find_all(articles, ".//MedlineJournalInfo/Country") %>% 
   xml_text()
 

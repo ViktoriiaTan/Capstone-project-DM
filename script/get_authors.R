e46ce79f49cd4f08e8f0d1231982bc6b88ca5766
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-27 17:12:22 +0200

    delete file

--- script/get_authors.R
+++
@@ -1,8 +0,0 @@
-#Get authors
-authors <- xml_find_all(summary, "//Author") %>%
-  lapply(function(x) {
-    last_name <- xml_find_all(x, ".//LastName") %>% xml_text(trim = TRUE)
-    initials <- xml_find_all(x, ".//Initials") %>% xml_text(trim = TRUE)
-    paste(last_name, initials, sep = " ")
-  }) %>%
-  paste(collapse = ", ")

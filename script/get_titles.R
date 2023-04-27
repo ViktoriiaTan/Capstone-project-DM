7031d04bef3cc827a82604ff3cb89f9c0b1661f6
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-27 17:09:02 +0200

    remove abstracts from file

--- script/get_titles.R
+++ script/get_titles.R
@@ -50,35 +50,3 @@ journal_t <- function(art){
   return(j_titles)
 }
 journal_title <- journal_t(articles)
-
-#Retrieve abstracts
-
-abstracts <- xml_find_all(articles, "//Article/Abstract") %>%
-  xml_text()
-
-abstract_function <- function(art){
-  
-  values <- c()
-  
-  articles <- xml_find_all(art, "//Article")
-  
-  for (i in 1:length(articles)) {
-    article <- articles[[i]]
-    abstract_element <- xml_find_first(article, "Abstract")
-    
-    if (is.null(abstract_element)){
-      values <- c(values, "No abstract")
-      next
-    }
-    
-    values <- c(values, xml_text(abstract_element))
-  }
-  return(values)
-}
-
-abstract <- abstract_function(articles)
-
-#Get country where the journal is published
-country <- xml_find_all(articles, ".//MedlineJournalInfo/Country") %>% 
-  xml_text()
-

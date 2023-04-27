702dfd23fc632aa955b75a585944adc4d987a497
ViktoriiaTan <viktantsiura@gmail.com>
2023-04-27 18:11:10 +0200

    creating main function that calls all others and creates df

--- script/mainfunc.R
+++ script/mainfunc.R
@@ -1,12 +1,41 @@
 # source the dependent functions from other files
-setwd("/Users/viktoriiatantsiura/Downloads/University_Luzern/Data_Mining/Capstone-project-DM/script")
+#setwd("...")
 source("pubmed_funcs.R")
-
-source("get_authors.R")
+source("abstracts.R")
+source("items.R")
 source("get_dates.R")
 source("get_titles.R")
+source("outlink.R")
 
-
-df <- data_frame("PMID" = art_ids, "Title" = article_title, "Abstract" = abstracts, 
-                 "Journal" = journal_title, "Country" = country, "Outlink" = linkout,
-                 "Article received" = pub_received, "Article accepted"= pub_accepted, "DOI"= doi)
+get_content <- function(db = "pubmed", query, retmax = 20) {
+  
+  # Retrieve PubMed article IDs
+  art_ids <- pubmed_ids(db = db, query = query, retmax = retmax)
+  print (art_ids)
+  # Retrieve PubMed article content
+  articles <- pubmed_content(ids = art_ids, db = db, retmode = "xml", rettype = "abstract")
+  article_title <- title(articles)
+  journal_title <- journal_t(articles)
+  
+  abstract <- abstract_function(articles)
+  
+  pub_received <- date_r(articles)
+  pub_accepted <- date_acc(articles)
+  print(pub_received )
+  print(pub_accepted )
+  country <- get_country(articles)
+  
+  authors <- get_authors(articles)
+  
+  linkout <- pubmed_linkout(db, art_ids)
+  
+  doi <- doi_results(articles)
+  
+  # Create data frame with results
+  df <- tibble("PMID" = art_ids, "Title" = article_title, "Abstract" = abstract, 
+                   "Journal" = journal_title, "Country" = country, "Outlink" = linkout,
+                   "Article received" = pub_received, "Article accepted"= pub_accepted, "DOI"= doi)
+  
+  return(df)
+}
+b<- get_content(db,query, 250)
